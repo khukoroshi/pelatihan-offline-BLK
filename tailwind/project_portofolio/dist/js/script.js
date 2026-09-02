4eac681524642fd074d482567e1e1cea8bf235ee
mangchubet <haikalfiransyah28@gmail.com>
2026-09-02 18:26:58 +0900

    membuat web portofolio menggunakan tailwindcss

---
+++ tailwind/project_portofolio/dist/js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+window.onscroll = function () {
+  const header = this.document.getElementById("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
