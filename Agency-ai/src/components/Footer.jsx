51e435d71ad55767e4e465cea16819fefaefdbe9
mangchubet <haikalfiransyah28@gmail.com>
2026-09-04 16:03:03 +0900

    hehe

--- Agency-ai/src/components/Footer.jsx
+++ Agency-ai/src/components/Footer.jsx
@@ -75,15 +75,21 @@ const Footer = ({ theme }) => {
       </motion.div>
       <hr className="border-gray-300 dark:border-gray-600 my-6" />
       {/* footer bottom */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-        viewport={{ once: true }}
-        className="pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap"
-      >
+      <div className="pb-6 text-sm text-gray-500 flex justify-center gap-4 flex-wrap">
         <p>Copyright 2026 &copy; agency.ai - All right reserved</p>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          viewport={{ once: true }}
+          className="flex items-center justify-between gap-4"
+        >
+          <img src={assets.facebook_icon} alt="" />
+          <img src={assets.twitter_icon} alt="" />
+          <img src={assets.instagram_icon} alt="" />
+          <img src={assets.linkedin_icon} alt="" />
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
