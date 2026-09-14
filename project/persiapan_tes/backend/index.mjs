ae352a17126a5be2ab38bf62639642be63814501
khukoroshi <haikalfiransyah28@gmail.com>
2026-09-14 23:09:39 +0900

    udah beres

--- project/persiapan_tes/backend/index.mjs
+++ project/persiapan_tes/backend/index.mjs
@@ -1,21 +1,11 @@
 import express from "express";
 import "dotenv/config";
-import db from "./db/db.mjs";
+import db from "./config/db.mjs";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
-
-// A simple SELECT query
-// try {
-//   const [results, fields] = await db.query("SELECT * FROM siswa");
-
-//   console.log(results); // results contains rows returned by server
-//   console.log(fields); // fields contains extra meta data about results, if available
-// } catch (err) {
-//   console.log(err);
-// }
 
 app.get("/", (req, res) => {
   res.status(200).send("Selamat Datang di SI Register");
