536bae35395baa6b73f9bd7c7cd2c6c6e0481fa8
khukoroshi <haikalfiransyah28@gmail.com>
2026-09-15 18:11:30 +0900

    udah ah

--- project/persiapan_tes/backend/index.mjs
+++ project/persiapan_tes/backend/index.mjs
@@ -1,86 +1,31 @@
 import express from "express";
 import "dotenv/config";
-import db from "./config/db.mjs";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import siswaRoutes from "./routes/siswaRoutes.mjs";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// konfigurasi khusus
+// const corsOption = {
+//   origin:'http://localhost:3000',
+//   methods: 'GET,POST',
+//   optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOption))
+
+// middleware
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/siswa", siswaRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).send("Selamat Datang di SI Register");
-  // res.send("Hello World");
-});
-
-app.get("/api/siswa", async (req, res) => {
-  try {
-    const [results] = await db.query("SELECT * FROM siswa");
-    res.status(200).json(results);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.get("/api/siswa/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const query = "SELECT * FROM siswa WHERE id = ?";
-    const [rows] = await db.execute(query, [id]);
-
-    if (rows.length === 0) {
-      return res.status(404).json({ message: "Siswa tidak ditemukan" });
-    }
-    res.status(200).json(rows);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.post("/api/siswa", async (req, res) => {
-  try {
-    const { nama, email, kelas } = req.body;
-    const query = "INSERT INTO siswa (nama, email, kelas) VALUES (?, ?, ?)";
-    const [result] = await db.execute(query, [nama, email, kelas]);
-
-    res.status(201).json({
-      message: "Siswa berhasil didaftarkan",
-      siswaId: result.insertId,
-    });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.put("/api/siswa/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { nama, email, kelas } = req.body;
-    const query =
-      "UPDATE siswa SET nama = ?, email = ?, kelas = ? WHERE id = ?";
-    const [result] = await db.execute(query, [nama, email, kelas, id]);
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: "Siswa tidak ditemukan" });
-    }
-    res.status(200).json({ message: "Siswa berhasil diperbarui" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.delete("/api/siswa/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const query = "DELETE FROM siswa WHERE id = ?";
-    const [result] = await db.execute(query, [id]);
-
-    if (result.affectedRows === 0) {
-      return res.status(404).json({ message: "Siswa tidak ditemukan" });
-    }
-    res.status(200).json({ message: "Siswa berhasil di kick" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  res.status(200).send("Selamat Datang di Sistem Informasi Registrasi Siswa");
 });
 
 app.listen(PORT, () => {
